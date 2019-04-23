e4ecfad6079b5da4711f3117cf288b22411741a9
Annia Forney <anniaforney@gmail.com>
2019-04-23 21:29:17 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,18 +2,17 @@
 class Shoe 
   attr_accessor :brand, :color, :material , :size , :condition
 
-def intialize (brand)
-  @brand = brand
-end 
+  def intialize (brand)
+    @brand = brand
+  end 
 
+  def shoe_repair 
+    puts "Shoe has been repaired"
+  end 
 
-def shoe_repair 
-  puts "Shoe has been repaired"
-end 
-
-def new_shoe 
-  puts "Shoe is in new condition"
-end 
+  def new_shoe 
+    puts "Shoe is in new condition"
+  end 
 
 end 
   
