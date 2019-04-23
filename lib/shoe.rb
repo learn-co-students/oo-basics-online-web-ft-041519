0243216f30180f3a0292d91d08361eff24cc8d7a
Annia Forney <anniaforney@gmail.com>
2019-04-23 21:28:06 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,27 +1,11 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :brand, :color, :material , :size 
-  attr_reader :condition
+  attr_accessor :brand, :color, :material , :size , :condition
 
 def intialize (brand)
   @brand = brand
 end 
 
-def size =(num)
-  @size = num 
-end 
-
-def size 
-  @size 
-end 
-
-def material=(material)
-  @material = material
-end 
-
-def condition=(condition)
-  @condition = condition
-end 
 
 def shoe_repair 
   puts "Shoe has been repaired"
